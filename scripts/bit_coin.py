20b3b2258749b4e321cf8ac49161d4b50ff7fc75
Craig McNeile <craig.mcneile@plymouth.ac.uk>
2026-03-24 15:41:15 +0000

    update

--- scripts/bit_coin.py
+++ scripts/bit_coin.py
@@ -7,6 +7,8 @@ Bitcoin as a function of *time!*
 """)
  
 #df = pd.read_csv("my_data.csv")
-df = pd.read_csv("data/btcusd_1-min_data.csv")
+#df = pd.read_csv("data/btcusd_1-min_data.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/cmcneile/streamlit_tutorial/refs/heads/main/data/btcusd_1-min_data.csv")
+
 st.line_chart(df["High"])
 
